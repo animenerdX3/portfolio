d9c4e2aa945293db19039d292c135affd03ace1d
Peter Gomes <petergomesbusiness@gmail.com>
2018-03-24 14:31:00 -0400

    Add files via upload

---
+++ Templates/gallery/js/images.js
@@ -0,0 +1,7 @@
+var imageGal = [
+	{galleryimage: 'images/gallery/picture1.jpg', gallerythumb: 'images/gallery/thumbs/picture1_thumb.jpg', gallerycaption: 'Text 1'}, 
+	{galleryimage: 'images/gallery/picture2.jpg', gallerythumb: 'images/gallery/thumbs/picture2_thumb.jpg', gallerycaption: 'Text 2'}, 
+	{galleryimage: 'images/gallery/picture3.jpg', gallerythumb: 'images/gallery/thumbs/picture3_thumb.jpg', gallerycaption: 'Text 3'}, 
+	{galleryimage: 'images/gallery/picture4.jpg', gallerythumb: 'images/gallery/thumbs/picture4_thumb.jpg', gallerycaption: 'Text 4'}
+];
+
